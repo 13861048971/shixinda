1d2249a87f0d516a381924c3dd309adb91d669f6
fox <786598303@qq.com>
2017-04-14 19:19:37 +0800

    修改帖子分类显示；

--- Public/js/admin/admin.js
+++ Public/js/admin/admin.js
@@ -77,4 +77,48 @@
 $(function(){
 	renderEditor();
 });
+$('.post-cate-table').on('click',function(e){
+	var _this=$(e.target);
+	var pid=_this.data('id');
+	if(_this[0].className=='icon-add'){
+		var url="/admin/user/postcatechildren/pid/"+_this.data('id');
+		$.ajax({
+			url:url,
+			type:'post',
+			dataType:'json',
+			success:function(data){
+				var sub=data.data.list;
+				_this.removeClass('icon-add').addClass('icon-sub');
 
+				for(k in sub){
+					var html='<tr class="pid'+sub[k].pid+'"><td>';
+					if(sub[k].num > 0){
+						html+='<span class="icon-add" data-id="'+sub[k].id+'" data-level="'+(_this.data('level')+1)+'"></span>';
+					}
+
+					html+='<td>'+sub[k].id+'</td><td>';
+
+					for(var i=0;i<_this.data('level');i++){
+						html+='&nbsp;&nbsp;&nbsp;&nbsp;';
+					}
+
+					html+=sub[k].name+'</td><td>'+sub[k].deep+'</td>';
+
+					if(sub[k].status > 0){
+						html+='<td class="green">显示</td>'
+					}else{
+						html+='<td>不显示</td>'
+					}
+					
+					html+='<td>'+sub[k].addTime+'/'+sub[k].updateTime+'</td><td width=200 class="handle"><a class="dialog add" dialog-lg="true" href="#" url="/admin/user/postCateEdit/pid/'+sub[k].id+'">添加子类</a><a class="dialog edit" dialog-lg="true" href="#" url="/admin/user/postCateEdit/id/'+sub[k].id+'">编辑</a><a class="ajaxDel del" href="#" url="/admin/user/postCateDel/id/'+sub[k].id+'">删除</a></td></tr>';
+					
+					_this.parents('tr').after(html);
+				}
+			}
+		})
+	}else if(_this[0].className=='icon-sub'){
+		_this.removeClass('icon-sub').addClass('icon-add');
+		selector='.pid'+pid;
+		$(selector).remove();
+	}
+});
