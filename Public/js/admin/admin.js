877a39fa87bb68701f5e92d75cd4c27de22cc67c
fox <786598303@qq.com>
2017-04-17 15:03:34 +0800

    导航管理分级添加；

--- Public/js/admin/admin.js
+++ Public/js/admin/admin.js
@@ -108,7 +108,7 @@ $('.post-cate-table').on('click',function(e){
 						html+='<td>不显示</td>';
 					}
 					
-					html+='<td>'+sub[k].addTime+'/'+sub[k].updateTime+'</td><td width=200 class="handle"><a class="dialog add" dialog-lg="true" href="#" url="/admin/user/postCateEdit/pid/'+sub[k].id+'">添加子类</a><a class="dialog edit" dialog-lg="true" href="#" url="/admin/user/postCateEdit/id/'+sub[k].id+'">编辑</a><a class="ajaxDel del" href="#" url="/admin/user/postCateDel/id/'+sub[k].id+'">删除</a></td></tr>';
+					html+='<td>'+sub[k].addTime+'</td><td>'+sub[k].updateTime+'</td><td width=200 class="handle"><a class="dialog add" dialog-lg="true" href="#" url="/admin/user/postCateEdit/pid/'+sub[k].id+'">添加子类</a> <a class="dialog edit" dialog-lg="true" href="#" url="/admin/user/postCateEdit/id/'+sub[k].id+'">编辑</a> <a class="ajaxDel del" href="#" url="/admin/user/postCateDel/id/'+sub[k].id+'">删除</a></td></tr>';
 					
 					_this.parents('tr').after(html);
 				}
@@ -139,7 +139,7 @@ $('.content-cate-table').on('click',function(e){
 						html+='<span class="icon-add" data-id="'+sub[k].id+'" data-level="'+(_this.data('level')+1)+'"></span>';
 					}
 
-					html+='</td><td>'+sub[k].id+'</td><td class="post-cate-name">';
+					html+='</td><td class="post-cate-name">';
 
 					for(var i=0;i<_this.data('level');i++){
 						html+='&nbsp;&nbsp;&nbsp;&nbsp;';
@@ -147,13 +147,55 @@ $('.content-cate-table').on('click',function(e){
 
 					html+=sub[k].name+'</td><td>'+sub[k].rank+'</td>';
 
-					if(sub[k].statusName > 0){
-						html+='<td class="green">显示</td>';
+					if(sub[k].status > 0){
+						html+='<td class="green">'+sub[k].statusName+'</td>';
 					}else{
-						html+='<td>禁用</td>';
+						html+='<td>'+sub[k].statusName+'</td>';
 					}
 					
 					html+='<td>'+sub[k].addTime+'</td><td>'+sub[k].updateTime+'</td><td width=200 class="handle"><a class="dialog add" dialog-lg="true" href="#" url="/admin/content/addContentChildren/id/'+sub[k].id+'/name/'+sub[k].name+'">添加子类</a>| <a class="dialog edit" dialog-lg="true" href="#" url="/admin/content/contentCateEdit/id/'+sub[k].id+'">编辑</a>| <a class="ajaxDel del" href="#" url="/admin/content/contentCateDel/id/'+sub[k].id+'">删除</a></td></tr>';
+					console.log(html);
+					_this.parents('tr').after(html);
+				}
+			}
+		})
+	}else if(_this[0].className=='icon-sub'){
+		_this.removeClass('icon-sub').addClass('icon-add');
+		selector='.pid'+pid;
+		$(selector).remove();
+	}
+});
+$('.navigation-table').on('click',function(e){
+	var _this=$(e.target);
+	var pid=_this.data('id');
+	if(_this[0].className=='icon-add'){
+		var url="/admin/content/navigationChildren/pid/"+_this.data('id');
+		$.ajax({
+			url:url,
+			type:'get',
+			dataType:'json',
+			success:function(data){
+				var sub=data.data.list;
+				_this.removeClass('icon-add').addClass('icon-sub');
+				for(k in sub){
+					var html='<tr class="'+_this.parents('tr')[0].className+'pid'+sub[k].pid+' "><td>';
+					if(sub[k].num > 0){
+						html+='<span class="icon-add" data-id="'+sub[k].id+'" data-level="'+(_this.data('level')+1)+'"></span>';
+					}
+
+					html+='</td><td class="post-cate-name">';
+
+					for(var i=0;i<_this.data('level');i++){
+						html+='&nbsp;&nbsp;&nbsp;&nbsp;';
+					}
+					html+=sub[k].name+'</td><td><img src="'+sub[k].logo+'" width=25></td><td>'+sub[k].url+'</td>';
+					if(sub[k].status > 0){
+						html+='<td class="green">'+sub[k].statusName+'</td>';
+					}else{
+						html+='<td>'+sub[k].statusName+'</td>';
+					}
+					
+					html+='<td>'+sub[k].rank+'</td><td width=200 class="handle"><a class="dialog add" dialog-lg="true" href="#" url="/admin/content/navigationChildrenEdit/name/'+sub[k].name+'/pid/'+sub[k].id+'">添加子类</a>| <a class="dialog edit" dialog-lg="true" href="#" url="/admin/content/navigationEdit/id/'+sub[k].id+'">编辑</a>| <a class="ajaxDel del" href="#" url="/admin/content/navigationDel/id/'+sub[k].id+'">删除</a></td></tr>';
 					console.log(html);
 					_this.parents('tr').after(html);
 				}
