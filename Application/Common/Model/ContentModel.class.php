f095b80e2b63a7df1cbbf4745b949ec17d0fd225
fox <786598303@qq.com>
2017-04-22 16:43:50 +0800

    二次初始化；

--- Application/Common/Model/ContentModel.class.php
+++ Application/Common/Model/ContentModel.class.php
@@ -32,7 +32,6 @@ class ContentModel extends BaseModel {
 	public function getInfo($id){
 	    $info = $this->find($id);
 	    if(!$info) return;
-	    $info = $this->parseRow($info);
 	    $tdkInfo = d('tdk')->getInfo($id);
 	   
 	    $info['tdk'] = $tdkInfo;
--- Public/home/js/custom.js
+++ Public/home/js/custom.js
@@ -1,19 +1,13 @@
-﻿
-
-
-(function ($) {
+﻿(function ($) {
     "use strict";
     var mainApp = {
         slide_fun: function () {
             $('#carousel-div').carousel({
                 interval: 4000 //TIME IN MILLI SECONDS
             });
-
         },
         wow_fun: function () {
-
             new WOW().init();
-
         },
         gallery_fun: function () {
             /*====================================
@@ -34,8 +28,6 @@
                 filterSelector: '.filter',
                 effects: ['fade'],
                 easing: 'snap',
-
-
             });
         },
        
@@ -77,5 +69,43 @@ $('.flexslider').flexslider({
     }
 });
 });
+$('.product-detail').on('click',function(e){
+    var node = $(e.target);
+    if(node[0].className == 'product-intro'){
+        return false;
+    }
+    if(node[0].nodeName == 'SPAN'){
+        node = node.parents('dt');
+    }
+    if(node[0].nodeName == 'A'){
+        node = node.parent('dd');
+    }
+    if(node.find('span')[0]){
+        var icon = node.find('span');
+        if(icon.css('transform') == 'matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)'){
+            icon.css('transform','rotate(0deg)');
+        }else{
+            icon.css('transform','rotate(90deg)');
+        }
+    }
 
+    if(node[0].nodeName == 'DT'){
+        node.siblings('dd').slideToggle(200);
+    }
+    if(node[0].nodeName == 'DD'){
+        var url = node.find('a').attr('href');
 
+        node.parents('.product-detail').find('dd').css('background-color','#fafafa');
+        node.css('background-color','#eee');
+
+        $.ajax({
+            url:url,
+            type:'get',
+            dataType:'json',
+            success:function(data){
+                $('.product-title').empty().append(data.data.title);
+                $('.product-content').empty().append(data.data.content);
+            }
+        });
+    }
+});
