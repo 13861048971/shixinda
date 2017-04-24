d611200c68872f99dcfb25c1cd72fcc2b1f871b0
chixin <13861048971@163.com>
2017-04-24 09:54:21 +0800

    登录和注册

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -180,6 +180,16 @@ class IndexController extends PublicController {
 	}
 	
 	//用户登录
+	public function login(){
+	   $this->display();
+	}
+	
+	//用户注册
+	public function regist(){
+	    $this->display();
+	}
+	
+	//
 	
 	
 }
