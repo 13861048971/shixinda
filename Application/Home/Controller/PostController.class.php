fe28dcaed75d2b2b3f20e56a2b7a353f59671b27
wangyanzhong <wyzhh2374591076@163.com>
2017-04-27 18:28:46 +0800

    论坛详情

--- Application/Home/Controller/PostController.class.php
+++ Application/Home/Controller/PostController.class.php
@@ -9,12 +9,16 @@ class PostController extends PublicController {
         foreach($list as $k1=>$v1){
             $list[$k1]['list'] = d('postCate')->getList(['pid'=>$list[$k1]['id'], 'status'=>'1']);
             foreach($list[$k1]['list'] as $k2=>$v2){
+                //dump($v2['id']);exit();
+                $id = d('postCate')->where(['pid'=>$v2['id']])->getField('id');
+                //dump($id);exit();
+                $idArr = [$v2['id'], $id];
                 $con = [
                         'add_time'     => ['gt', strtotime(date("Y-m-d"))],
-                        'post_cate_id' => $v2['id']
+                        'post_cate_id' => ['in', $idArr] 
                        ];
                 $list[$k1]['list'][$k2]['todayPostNum'] = d('post')->where($con)->count();
-                $list[$k1]['list'][$k2]['mainPostNum'] = d('post')->where(['post_cate_id' => $v2['id']])->count();
+                $list[$k1]['list'][$k2]['mainPostNum'] = d('post')->where(['post_cate_id' => ['in', $idArr]])->count();
             }
         } 
         $this->assign('hotList', $hotList);
@@ -29,25 +33,29 @@ class PostController extends PublicController {
 	    $post_cate_id3 = $_GET['post_cate_id3'];
 	    //三级分类列表
 	    $childrenList = d('postCate')->getList(['pid'=>$post_cate_id2, 'status'=>1]);
-	    foreach($childrenList as $k=>$v){
-	        $childrenList[$k]['count'] = d('post')->where(['post_cate_id'=>$v['id']])->count();
-	        $idArr[$k] = $v['id'];
+	    if($childrenList){
+	        foreach($childrenList as $k=>$v){
+	            $childrenList[$k]['count'] = d('post')->where(['post_cate_id'=>$v['id']])->count();
+	            $idArr[$k] = $v['id'];
+	        }
+	        //二级分类名称
+	        $name = d('postCate')->where(['id'=>$post_cate_id2])->getField('name');
+	        $where['post_cate_id'] = array('in', $idArr);
+	        $map['post_cate_id'] = array('eq', $post_cate_id2);
+	        $map['_complex'] = $where;
+	        $map['_logic'] = 'or';
+	        if($post_cate_id2&&$post_cate_id3){
+	            $data = d('post')->getPageList(['post_cate_id'=>$post_cate_id3, 'status'=>'1'], '*', 'add_time desc', 3);
+	        }elseif($post_cate_id2){
+	            $data = d('post')->getPageList([$map, 'status'=>'1'], '*', 'add_time desc', 3);
+	        }
+	        $this->assign('childrenList', $childrenList);
 	    }
-	    //二级分类名称
-	    $name = d('postCate')->where(['id'=>$post_cate_id2])->getField('name');
-	    $where['post_cate_id'] = array('in', $idArr);
-	    $map['post_cate_id'] = array('eq', $post_cate_id2);
-	    $map['_complex'] = $where;
-	    $map['_logic'] = 'or';
-	    if($post_cate_id2&&$post_cate_id3){
-	        $data = d('post')->getPageList(['post_cate_id'=>$post_cate_id3, 'status'=>'1'], '*', 'add_time desc', 3);
-	    }elseif($post_cate_id2){
-	        $data = d('post')->getPageList([$map, 'status'=>'1'], '*', 'add_time desc', 3);
-	        
+	    else{
+	        $data = d('post')->getPageList(['post_cate_id'=>$post_cate_id2, 'status'=>'1'], '*', 'add_time desc', 3);
 	    }
 	    
-	    //dump($data['list']);exit();
-	    $this->assign('childrenList', $childrenList);
+	    
 	    $this->assign('name', $name);
 	    $this->assign('post_cate_id2', $post_cate_id2);
 	    $this->assign('post_cate_id3', $post_cate_id3);
@@ -60,9 +68,11 @@ class PostController extends PublicController {
 	//获取帖子详情
 	public function postDetail(){
 	    $id = $_GET['id'];
-	    $post_info = d('post')->getInfo($id);
+	    $userRow = d('post')->where(['id'=>$id])->find();
+	    
+	    $postRow = d('post')->getInfo($id);
 	    $this->click('post',$id);//访问量+1
-	    $this->assign('info', $post_info);
+	    $this->assign('postRow', $postRow);
 	    $this->display();
 	}
 	
